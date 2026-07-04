e9b9b18b5c25c5d49f8d53122a35e44646727feb
arenkotanya-byte <arenkotanya@gmail.com>
2026-07-04 20:33:00 +0300

    Update model.py

--- model.py
+++ model.py
@@ -25,8 +25,8 @@ def train_regression_model(X_train: ArrayLike, y_train: ArrayLike) -> LinearRegr
 
     """
 
-    # TODO: your code here
-
+    model = LinearRegression()
+    model.fit(X_train, y_train)
     return model
 
 def save_regression_model(model: LinearRegression, filename: str = "linear_regression_model.joblib"):
@@ -42,7 +42,7 @@ def save_regression_model(model: LinearRegression, filename: str = "linear_regre
 
     """
     
-    # TODO: your code here
+    dump(model, filename)
 
 def evaluate_regression_model(model: LinearRegression, X_test: ArrayLike, y_test: ArrayLike):
     """
@@ -59,8 +59,8 @@ def evaluate_regression_model(model: LinearRegression, X_test: ArrayLike, y_test
 
     """
     
-    # TODO: your code here
-
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
     print(f"Mean Squared Error: {mse}")
 
 def save_initial_datasets(X: ArrayLike, y: ArrayLike):
@@ -78,7 +78,8 @@ def save_initial_datasets(X: ArrayLike, y: ArrayLike):
     X_filename = "X.joblib"
     y_filename = "y.joblib"
     
-    # TODO: your code here
+    dump(X, X_filename)
+    dump(y, y_filename)
 
 if __name__ == '__main__':
     # Generate a dataset
